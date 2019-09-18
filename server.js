a86c3cb3e0ff6a837a6ee59ffc98f17f4c73243a
Nguyen Tu Khai <tukhai.nguyen@edgeprop.sg>
2019-09-18 23:39:26 +0800

    Add routing middleware and koa server

--- server.js
+++ server.js
@@ -16,4 +16,14 @@ const handle = app.getRequestHandler();
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
 
-app.prepare().then(() => {});
+app.prepare().then(() => {
+	const server = new Koa();
+	server.use(session(server));
+	server.keys = [SHOPIFY_API_SECRET_KEY];
+	server.use(async (ctx) => {
+		await handle(ctx.req, ctx.res);
+		ctx.respond = false;
+		ctx.res.statusCode = 200;
+		return;
+	});
+});
