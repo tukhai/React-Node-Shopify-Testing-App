0ea8c7a7651c48129350a196333f48197bc879b1
Nguyen Tu Khai <tukhai.nguyen@edgeprop.sg>
2019-09-20 14:06:23 +0800

    Add Title Bar (with button) & Resource Picker (just the built-in popup UI)

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,60 @@
 // import { Layout, Page, TextStyle } from '@shopify/polaris';
 import { EmptyState, Layout, Page } from '@shopify/polaris';
-
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-const Index = () => (
-	<Page>
-		<Layout>
-			{/* <TextStyle variation="positive">
-				Sample app using React and Next.js
-			</TextStyle> */}
+// const Index = () => (
+class Index extends React.Component {
+	state = { open: false };
+	render() {
+		return (
+			<Page>
+				<TitleBar
+					primaryAction={{
+						content: 'Select products',
+						onAction: () => console.log('clicked to button on title bar'),
+					}}
+				/>
 
-			<EmptyState
-				heading="Discount your products temporarily"
-				action={{
-					content: 'Select products',
-					onAction: () => console.log('clicked'),
-				}}
-				image={img}
-			>
-				<p>Select products to change their price temporarily.</p>
-			</EmptyState>
-		</Layout>
-	</Page>
-);
+				<ResourcePicker
+					resourceType="Product"
+					showVariants={false}
+					open={this.state.open}
+					onSelection={(resources) => this.handleSelection(resources)}
+					onCancel={() => this.setState({ open: false })}
+				/>
+
+				<Layout>
+					{/* <TextStyle variation="positive">
+						Sample app using React and Next.js
+					</TextStyle> */}
+
+					<EmptyState
+						heading="Discount your products temporarily"
+						action={{
+							content: 'Select products',
+							// onAction: () => console.log('clicked'),
+							onAction: () => this.setState({ open: true }),
+						}}
+						image={img}
+					>
+						<p>Select products to change their price temporarily.</p>
+					</EmptyState>
+				</Layout>
+			</Page>
+		);
+	}
+
+	handleSelection = (resources) => {
+		this.setState({ open: false });
+		console.log(resources);
+	}
+};
 
 export default Index;
+
+
+
+
+
+
