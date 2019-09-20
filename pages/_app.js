784dff0a0d56e6d2c7eaa9682506b7bf2a11b387
Nguyen Tu Khai <tukhai.nguyen@edgeprop.sg>
2019-09-21 00:21:38 +0800

    Configure the Apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
 import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+	fetchOptions: {
+		credentials: 'include'
+	},
+});
 
 class MyApp extends App {
 	render() {
@@ -17,7 +25,9 @@ class MyApp extends App {
 				</Head>
 				<Provider config={config}>
 					<AppProvider>
-						<Component {...pageProps} />
+						<ApolloProvider client={client}>
+							<Component {...pageProps} />
+						</ApolloProvider>
 					</AppProvider>
 				</Provider>
 			</React.Fragment>
